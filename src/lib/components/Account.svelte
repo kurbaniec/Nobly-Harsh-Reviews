80d1a34e8c65db4d4e703a6920e691580fef93c1
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2024-04-20 21:54:14 +0200

    Working on simple navbar

---
+++ src/lib/components/Account.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+</script>
+
+<div class="dropdown">
+	<span>Account</span>
+	<div class="dropdown-content">
+		<p>Hello World</p>
+		<p>Hello World</p>
+	</div>
+</div>
+
+<style lang="postcss">
+	.dropdown {
+		position: relative;
+		display: inline-block;
+		@apply p-2;
+		@apply border border-foreground border-opacity-20;
+		@apply border-2 rounded-md;
+	}
+
+	.dropdown-content {
+		/* display: none;
+		position: absolute;
+		min-width: 160px;
+		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+		padding: 12px 16px;
+		z-index: 1;
+		width: 100%;
+		overflow: auto;
+		@apply bg-background;
+		@apply border border-foreground border-opacity-20;
+		@apply border-2 rounded-md; */
+		display: none;
+		@apply absolute right-0 w-24 mt-2 py-2 shadow-xl rounded z-50;
+		@apply bg-background;
+		@apply border border-foreground border-opacity-20;
+		@apply border-2 rounded-md;
+	}
+
+	.dropdown:hover .dropdown-content {
+		display: block;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
 	import '../app.css';
 	import ThemePicker from '$lib/components/ThemePicker.svelte';
+	import Account from '$lib/components/Account.svelte';
 
 	let { data, children } = $props();
 	let theme: string | undefined = $state(data.theme);
 </script>
 
 <div data-theme={theme} class="theme-body">
-	<div class="container mx-auto flex-col h-screen">
+	<!-- <header class="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-stretch">
+		<div class="md:ml-auto md:mr-auto flex flex-wrap items-stretch text-base justify-center">
+			<div class="mr-5 h-full"><ThemePicker bind:theme /></div>
+			<div class="mr-5"><Account /></div>
+		</div>
+	</header> -->
+	<header class="theme-header">
+		<h1 class="p-2">Website</h1>
 		<ThemePicker bind:theme />
+		<Account />
+	</header>
+	<div class="container mx-auto flex-col p-2">
 		{@render children()}
 	</div>
 </div>
 
 <style lang="postcss">
 	.theme-body {
-		@apply bg-background;
+		@apply bg-background h-screen;
+	}
+
+	.theme-header {
+		@apply p-5 flex gap-2 justify-end flex-col sm:flex-row;
+		@apply border-b-2 border-foreground border-opacity-20;
 	}
 
 	:global(h1) {
 		@apply text-foreground text-xl;
 	}
-	:global(p) {
+	:global(p, span) {
 		@apply text-foreground;
 	}
 
